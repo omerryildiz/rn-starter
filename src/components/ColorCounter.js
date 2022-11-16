edcb9f955a1329de7ffe4a35c83662f63347360b
ÖMER YILDIZ <54753230+omerryildiz@users.noreply.github.com>
2022-11-16 15:56:30 +0300

    L2

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,21 +1,22 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
-const ColorCounter = ({color}) => {
-    const [colors, setColors] = useState([]);
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
 
     return (
         <View>
             <View>
                 <Text>{color}</Text>
                 <Button
+                    onPress={(() => onIncrease())}
                     title={`"${color} +"`}
                 />
                 <Button
+                    onPress={(() => onDecrease())}
                     title={`"${color} -"`}
-                    />
+                />
             </View>
-          
+
         </View>
 
     );
